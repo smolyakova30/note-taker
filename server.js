d40432dfe8390eb5e1651276386df02b73b05fbb
Olga <o.smolyakova30@gmail.com>
2021-08-12 18:08:35 -0500

    added variable to server.js, created nodeFunctions.js to add function to notes

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
