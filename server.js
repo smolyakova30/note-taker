75ff1bedc8cc04df87a68e1fdf1d8c8c5c5f99b2
Olga <o.smolyakova30@gmail.com>
2021-08-15 13:28:41 -0500

    Add Heroku

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require('path');
 
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
+//const fs = require('fs');
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
